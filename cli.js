19f60281c0a89173559b9086d5629adbc9211fc7
alestor123 <alestoraldous@gmail.com>
2021-07-02 22:21:52 +0530

    added main cli code

--- cli.js
+++ cli.js
@@ -2,15 +2,23 @@
 const chalk = require('chalk')
 const { userInfo } = require('os')
 const prompt = require('prompt-sync')()
+const setyear = require('./App')
 const usernamee = userInfo().username
-const { mkdirSync } = require('fs')
-const questions = [{ field: 'username', question: 'Github Username(' + usernamee + ') : ', defaultValue: usernamee, nolengthValidation: true }, { field: 'brname', defaultValue: 'main', question: 'Branch (main) : ', nolengthValidation: true }, { field: 'year', defaultValue: 1995, question: 'Year (1995) : ', nolengthValidation: true }]
+const { mkdirSync,writeFileSync } = require('fs')
+const { join,resolve } = require('path')
+const questions = [{ field: 'username', question: 'Github Username(' + usernamee + ') : ', defaultValue: usernamee, nolengthValidation: true }, { field: 'brname', defaultValue: 'main', question: 'Branch (main) : ', nolengthValidation: true }, { field: 'year', defaultValue: 1995, question: 'Year (1995) : ', nolengthValidation: true }, { field: 'repo', defaultValue: '1995', question: 'Repository (1995) : ', nolengthValidation: true }]
 try {
-    console.log(promptAns())
+if(prompt('Are you sure because its not that great (Y/n) :','y').toLocaleLowerCase()==='y'){
+    var {year,brname,username,repo}= promptAns()
+    mkdirSync(repo)
+    writeFileSync(join(repo,'README.md'),'# '+year)
+    console.log(setyear(username, Number(year), brname,repo,resolve(repo)))
     console.log(chalk.green('Done!!'))
 }
+else console.log(chalk.redBright.bold('Aborted !'))
+}
 catch (e) {
-    console.log(chalk.red.bold('Oops we got a problem'))
+    console.log(chalk.redBright.bold('Oops we got a problem'))
 }
 function promptAns () {
     const answers = {}
--- run.js
+++ run.js
@@ -1,2 +1,2 @@
 const setyear = require('./App')
-console.log(setyear('alestor123', 2000, 'master'))
+console.log(setyear('alestor123', 2000, 'master','2000','../2000/'))
